5fdb0ce0d2cbd5f4048034a4dfaa19b6bd4617e0
Yerkoo <123399269+Yerkoo@users.noreply.github.com>
2023-03-09 11:17:27 +0600

    Add files via upload

---
+++ PP2/lab4/dsjj.py
@@ -0,0 +1,1 @@
+from dcjj import gcdt
---
+++ PP2/lab4/generator.py
@@ -0,0 +1,16 @@
+n = int(input())
+def Fibonacci(length):
+    first = 0
+    second = 1
+    print(first, second, end =" ")
+    length -= 2
+    while length > 0:
+        print(first + second, end =" ")
+        temp = second
+        second = first + second
+        first = temp
+        length -= 1
+
+if __name__ == "__main__":
+    Fibonacci(n)
+    pass
